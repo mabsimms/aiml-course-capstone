0cc2a5cd36d02f3b897d3d0f7450797d1341a01a
Mark Simms <masimms@outlook.com>
2026-08-09 16:06:54 -0700

    Bringing rtx5090 friendly tensorflow build into repo as lfs

---
+++ dependencies/build-tensorflow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build Tensorflow (this takes about 2-4 hours)
+bazel build //tensorflow/tools/pip_package:wheel   \
+	--repo_env=WHEEL_NAME=tensorflow   \
+	--repo_env=ML_WHEEL_VERSION_SUFFIX=+rtx5090.cuda13   \
+	--config=cuda_wheel   \
+	--config=opt   \
+	-c opt   \
+	--repo_env=HERMETIC_CUDA_VERSION=13.0.0   \
+	--repo_env=HERMETIC_CUDNN_VERSION=9.12.0   \
+	--repo_env=HERMETIC_CUDA_COMPUTE_CAPABILITIES=12.0   \
+	--@local_config_cuda//:cuda_compiler=nvcc
